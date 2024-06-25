c64c59a8b43358250871bdb6768eba76571223c4
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 17:39:45 +0300

    object response

--- app/api/input/route.js
+++ app/api/input/route.js
@@ -3,15 +3,6 @@ import { NextResponse } from 'next/server';
 
 export default async function GET(request) {
 
-    try {
-        await sql`INSERT INTO Basliks (title) VALUES ("recep ivedik"),
-         ("tiktok'ta dans eden gençler"),
-         ("arda güler'in gürcistan'a attığı gol")
-;`;
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500 });
-    }
-
     return (
         <form>
             <textarea cols={40} rows={10} placeholder={"entry girin"}></textarea>
