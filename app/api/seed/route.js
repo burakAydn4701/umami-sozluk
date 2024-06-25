6d4a1bc1f968a22b2dabf70b69eda0f5b12f6ec7
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 16:56:13 +0300

    m

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -13,6 +13,10 @@ export async function GET(request) {
             author varchar(255) NOT NULL,
             FOREIGN KEY (baslik_id) REFERENCES Basliks (id) ON DELETE CASCADE
              );`;
+            await sql`INSERT INTO Basliks (title) VALUES ("recep ivedik"),
+            ("tiktok'ta dans eden gençler")
+            ("arda güler'in gürcistan'a attığı gol")"`
+
         return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
