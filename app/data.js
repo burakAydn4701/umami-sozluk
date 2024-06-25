20b6ec3207be43a67a6d4444b1dfffe09afb940f
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 13:35:22 +0300

    m

---
+++ app/data.js
@@ -0,0 +1,51 @@
+
+export default function Data() {
+    return [
+        {
+            id: 0,
+            title: "recep ivedik",
+            entries: [
+                {
+                    no: 0,
+                    text: "yarısında çıktığım filmdir.",
+                    author: "sozlukcu47",
+                    upvote: 0,
+                    downvote: 0
+                },
+                {
+                    no: 1,
+                    text: "sandalyeden düşüren film",
+                    author: "prostethnicsth",
+                    upvote: 0,
+                    downvote: 0
+                }
+            ]
+        },
+        {
+            id: 1,
+            title: "tiktok'ta dans eden gençler",
+            entries: [
+                {
+                    no: 0,
+                    text: "gümbür gümbür geliyor denen z kuşağı bunlar işte. yazık.",
+                    author: "boomer35",
+                    upvote: 0,
+                    downvote: 0
+                }
+            ]
+        },
+        {
+            id: 2,
+            title: "arda güler'in gürcistan'a attığı gol",
+            entries: [
+                {
+                    no: 0,
+                    text: "bombastiktir.",
+                    author: "robertobaggio",
+                    upvote: 0,
+                    downvote: 0
+                }
+            ]
+        }
+    ]
+}
