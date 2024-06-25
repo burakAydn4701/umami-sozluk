c72b485f7e1c2665f2728e0bc7fd7b07c95ffbec
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 16:48:19 +0300

    initial başlıks

---
+++ app/api/seed/initialData.js
@@ -0,0 +1,19 @@
+
+export const initialBasliks = [
+    {
+        id: 0,
+        title: "recep ivedik"
+    },
+    {
+        id: 1,
+        title: "tiktok'ta dans eden gençler"
+    },
+    {
+        id: 2,
+        title: "arda güler'in gürcistan'a attığı gol"
+    }
+]
+
+export const initialEntries = [
+
+]
