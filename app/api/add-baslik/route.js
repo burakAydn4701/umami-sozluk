8ccf80d7788d44d7f96dd54c70e1d25db66f1f06
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 17:21:11 +0300

    single quotes

--- app/api/add-baslik/route.js
+++ app/api/add-baslik/route.js
@@ -4,9 +4,9 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
 
     try {
-        await sql`INSERT INTO Basliks (title) VALUES ("recep ivedik"),
-        ("arda güler'in gürcistan'a attığı gol"),
-        ("tiktok'ta dans eden gençler");`;
+        await sql`INSERT INTO Basliks (title) VALUES ('recep ivedik'),
+        ('arda güler\'in gürcistan\'a attığı gol'),
+        ('tiktok\'ta dans eden gençler');`;
         return NextResponse.json({ message: "Rows inserted successfully" }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
