89813983c3b8f98431102aff9ef2f6441fcf4743
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-25 16:39:03 +0300

    export

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,56 +1,7 @@
 import {sql} from "@vercel/postgres";
 import {NextResponse} from "next/server";
 
-async function GET(request) {
-    const basliks = [
-        {
-            id: 0,
-            title: "recep ivedik",
-            entries: [
-                {
-                    no: 0,
-                    text: "yarısında çıktığım filmdir.",
-                    author: "sozlukcu47",
-                    upvote: 0,
-                    downvote: 0
-                },
-                {
-                    no: 1,
-                    text: "sandalyeden düşüren film",
-                    author: "prostethnicsth",
-                    upvote: 0,
-                    downvote: 0
-                }
-            ]
-        },
-        {
-            id: 1,
-            title: "tiktok'ta dans eden gençler",
-            entries: [
-                {
-                    no: 0,
-                    text: "gümbür gümbür geliyor denen z kuşağı bunlar işte. yazık.",
-                    author: "boomer35",
-                    upvote: 0,
-                    downvote: 0
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "arda güler'in gürcistan'a attığı gol",
-            entries: [
-                {
-                    no: 0,
-                    text: "bombastiktir.",
-                    author: "robertobaggio",
-                    upvote: 0,
-                    downvote: 0
-                }
-            ]
-        }
-    ]
-
+export async function GET(request) {
     try {
         const result =
             await sql`CREATE TABLE Basliks ( id SERIAL PRIMARY KEY,
