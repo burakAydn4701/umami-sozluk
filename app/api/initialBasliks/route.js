133533b151e5528a4a02b324b869f60c46598f63
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2024-06-26 16:24:03 +0300

    add rows-3

--- app/api/initialBasliks/route.js
+++ app/api/initialBasliks/route.js
@@ -1,17 +1,14 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-import {sql} from "@vercel/postgres";
-import {NextResponse} from "next/server";
 export async function GET(request) {
+
     try {
-        await sql`INSERT INTO basliks (title) VALUES ("recep ivedik"), ("tiktok'ta dans eden gençler"), ("arda güler'in gürcistan'a attığı gol");`;
+        await sql`INSERT INTO basliks (title) VALUES ("recep ivedik");`;
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
 
-    return NextResponse.json({ request }, { status: 200 });
+    const basliks = await sql`SELECT * FROM basliks;`;
+    return NextResponse.json({ pets }, { status: 200 });
 }
-
-//VALUES
-//
-//         ;
-
